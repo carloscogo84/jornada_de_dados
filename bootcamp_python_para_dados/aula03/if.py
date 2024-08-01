c5febeff32a03cd53b2d3440cfb6669cad0ec1c1
carloscogo84 <carloscogo84@gmail.com>
2024-08-01 10:53:26 +0100

    adicionado exemplo IF

---
+++ bootcamp_python_para_dados/aula03/if.py
@@ -0,0 +1,10 @@
+# if e else
+velocidade = 111
+
+if velocidade > 110:
+    print("Velocidade acima do permitido")
+    print("favor reduzir a velocidade")
+elif velocidade < 60:
+    print("favor dirigir acima da velocidade de 80km/h")
+else:
+    print("velocidade OK")
