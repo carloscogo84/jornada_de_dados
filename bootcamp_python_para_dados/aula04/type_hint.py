427565e6aee68db809330270cbf3ea5249bdce40
carloscogo84 <carloscogo84@gmail.com>
2024-08-04 10:36:50 +0100

    adicionado Type Hint

---
+++ bootcamp_python_para_dados/aula04/type_hint.py
@@ -0,0 +1,15 @@
+# Sem Type Hint
+
+# idade = 30
+# altura = 1.75
+# nome = "Alice"
+# is_estudante = True
+
+# com Type Hint
+# Boa pratica
+# Nao esta fazendo uma tipagem estatica
+
+idade: int = 30
+altura: float = 1.75
+nome: str = "Alice"
+is_estudante: bool = True
